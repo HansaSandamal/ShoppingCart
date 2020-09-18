68d93f8c09763111808b15fe0a60e5e38a0c2e58
HansaSandamal <hansasandamal@gmail.com>
2020-09-18 17:57:34 +0530

    odify home page

--- home.php
+++ home.php
@@ -5,11 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+    .head{
+        color:red;
+
+
+
+        
+    }
+    .select{
+        
+    }
+    </style>
 </head>
 <body>
+    <div class="head">
+        <h1>Roma Production</h1>
+    </div>
     <div class="select">
+
     <a href="login.php"><input type="button" value="login"></a>
     <a href="SignUp.php"><input type="button" value="SignUp"></a>
+
 
     </div>
 </body>
