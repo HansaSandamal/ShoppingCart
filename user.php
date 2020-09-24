6cb3ab58fbe9c7fc13ac5438b16a0c6d85b52033
HansaSandamal <hansasandamal@gmail.com>
2020-09-24 13:23:23 +0530

    modifing

--- user.php
+++ user.php
@@ -1,82 +1,88 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-        
-        padding:0;
-        margin:0;
-    }
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial;}
 
 .header {
   text-align: center;
   padding: 32px;
 }
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
 
-.row {
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
   display: -ms-flexbox; /* IE 10 */
   display: flex;
   -ms-flex-wrap: wrap; /* IE 10 */
   flex-wrap: wrap;
   padding: 0 4px;
 }
-
-/* Create two equal columns that sits next to each other */
-.column {
-  -ms-flex: 50%; /* IE 10 */
-  flex: 50%;
-  padding: 0 4px;
-}
-
 .column img {
   margin-top: 8px;
   vertical-align: middle;
 }
-
-/* Style the buttons */
-.btn {
-  border: none;
-  outline: none;
-  padding: 10px 16px;
-  background-color: #f1f1f1;
-  cursor: pointer;
-  font-size: 18px;
+.column {
+  -ms-flex: 25%; /* IE 10 */
+  flex: 25%;
+ 
+  padding: 0 4px;
 }
 
-.btn:hover {
-  background-color: #ddd;
-}
-
-.btn.active {
-  background-color: #666;
-  color: white;
-}
-
-
-
-
-    </style>
+</style>
 </head>
 <body>
 
 
 
 
-<!-- Header -->
 <div class="header" id="myHeader">
   <h1>PLANTS</h1>
   <p>Please Select your Plant Category</p>
-  <button class="btn" onclick="one()">cooking plants</button>
-  <button class="btn active" onclick="two()">colourful plants</button>
-  <button class="btn" onclick="three()">aquatic plants</button>
+  <div class="tab">
+  <button class="tablinks" onclick="openTab(event, 'cooking plants')">cooking plants</button>
+  <button class="tablinks" onclick="openTab(event, 'colourful plants')">colourful plants</button>
+  <button class="tablinks" onclick="openTab(event, 'aquatic plants')">aquatic plants</button>
+</div>
 </div>
 
-<!-- Photo Grid -->
-<div class="row"> 
+
+
+
+
+<div class="tabcontent" id="cooking plants"> 
   <div class="column">
     <img src="images/items/cooking plants/Abelmoschus-esculentus-–-Bandakka-LP-S-Home-garden-400x400.jpg" style="width:100%">
     <img src="images/items/cooking plants/Allium-ascalonicum-–-rathu-lunu-LP-S-Medi-400x400.jpg" style="width:100%">
@@ -109,53 +115,108 @@
 
 
 
+<div class="tabcontent" id="colourful plants"> 
+  <div class="column">
+    <img src="images/items/colourful plants/Acalypha-godseffiana-Heterophylla-–-LP-S-Boder-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Alternanthera-dentata-–-Joyweed-LP-S-home-garden-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Amaranthus-paniculatus-–-Rana-tempala-LP-S-Medi-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Bougainvillea-spectabilis-nyctaginaceae-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Cassia-fistuala-Ehala-T-L-W-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Congea-velutina-Pink-Sandpaper-Vine-Cr-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Cordyline-fruticosa-–-Plam-lily-LP-L-400x400.jpg" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="images/items/colourful plants/Dracaena-reflexa-Dracaena-LP-L-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Impatiens-balsamina-–-Kudalu-kola-LP-S-Low-inter-land-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Ipomoea-horsfalliae-princess-vine-Cr-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Ixora-coccinea-Ratmal-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Malpighia-glabra-Barbados-cherry-–LP-S-Boder-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/Mesua-ferrea-Naa-T-M-400x400.jpg" style="width:100%">
+    <img src="images/items/colourful plants/syzygium-campanulatum-christina-LP-L-1-400x400.jpg" style="width:100%">
+  </div>  
+  
+  <div class="column">
+    <img src="images/items/cooking plants/psophocarpus-tetragonolobus-Dambala-Climb-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/raphanus-sativus-Radish-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/Sauropus-androgynus-Japan-Batu-T-S-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/Sesbania-grandiflora-–-Katurumurunga-T-S-Home-Garden-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/Solanum-lycopersicum-tomatoes-LP-S-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/Solanum-melongena-Brinjal-LP-S-400x400.jpg" style="width:100%">
+    <img src="images/items/cooking plants/Solanum-melongena-elabatu-LP-S-400x400.jpg" style="width:100%">
+  </div>
+  
+  
+  
+  
+  <!-- <div class="column">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+    <img src="images/items/colourful plants/" style="width:100%">
+  </div> -->
+
+</div>
 
 
+
+<div class="tabcontent" id="aquatic plants"> 
+  <div class="column">
+    <img src="images/items/aquatic plants/Aeschynomene-aspera-Mahadhiya-siyabala-FAQ-1-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Aponogeton-crispus-Kekatiya-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Azolla-pinnata-azolla-FAQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Eclipta-alba-Kekirindiya-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Eichhornia-crassipes-Japan-Jabara-FAQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Limnanthemum-indicum-Kumudu-Aq-Indi-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nelumbo-nucifera-Nelum-AQ-400x400.jpg" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="images/items/aquatic plants/Nymphaea-lotus-Olu-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphaea-lutea-Kaha-Olu-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphaea-nouchali-Nil-Manel-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphaea-rubra-Rathu-Olu-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphaea-salphurea-La-Kaha-Olu-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphea-stellate-Manel-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Nymphoides-hydrophylla-heen-abhala-AQ-400x400.jpg" style="width:100%">
+  </div>  
+  <div class="column">
+    <img src="images/items/aquatic plants/Trapa-bispinosa-ikiliya-AQ-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/raphanus-sativus-Radish-400x400.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/Victoria-amazonica-giant-water-lily-AQ-400x400.jpg" style="width:100%">
+    <!-- <img src="images/items/aquatic plants/.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/.jpg" style="width:100%">
+    <img src="images/items/aquatic plants/.jpg" style="width:100%"> -->
+  </div>
+
+</div>
 
 
 <script>
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
-var elements2 = document.getElementsByClassName("column2");
+function openTab(evt, categoryName) {
+  var i, tabcontent, tablinks,elements;
+  
+  tabcontent = document.getElementsByClassName("tabcontent");
 
-// Declare a loop variable
-var i;
-
-// Full-width images
-function one() {
- for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    
   }
-}
-
-// Two images side by side
-function two() {
- for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+ 
+  
+  
   }
-}
+  document.getElementById(categoryName).style.display = "block";
 
-// Four images side by side
-function three() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
-  }
-}
-
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myHeader");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+ 
+  evt.currentTarget.className += " active";
 }
 </script>
-
+   
 </body>
-</html>
+</html> 
